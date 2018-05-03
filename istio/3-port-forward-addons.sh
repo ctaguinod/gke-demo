69ca5e928cca80c9a24f138ca0c63b213127e938
ctaguinod <ctaguinod@google.com>
2018-05-03 16:01:35 +0800

    fix 3-port-forward-addons.sh

--- istio/3-port-forward-addons.sh
+++ istio/3-port-forward-addons.sh
@@ -13,18 +13,12 @@ SERVICEGRAPH_POD_NAME=`kubectl -n istio-system get pod -l app=servicegraph -o js
 GRAFANA_POD_NAME=`kubectl -n istio-system get pod -l app=grafana -o jsonpath='{.items[0].metadata.name}'`
 PROMETHEUS_POD_NAME=`kubectl -n istio-system get pod -l app=prometheus -o jsonpath='{.items[0].metadata.name}'`
 
-echo $ISTIO_VERSION
-echo $ZIPKIN_POD_NAME
-echo $SERVICEGRAPH_POD_NAME
-echo $GRAFANA_POD_NAME
-echo $PROMETHEUS_POD_NAME
-
-#kubectl -n istio-system port-forward $(ZIPKIN_POD_NAME) 9411:9411 & 
+kubectl -n istio-system port-forward $ZIPKIN_POD_NAME 9411:9411 & 
 echo "Zipkin URL/Port: http://localhost:9411"
-#kubectl -n istio-system port-forward $(GRAFANA_POD_NAME) 3000:3000 &
+kubectl -n istio-system port-forward $GRAFANA_POD_NAME 3000:3000 &
 echo "Grafana URL/Port: http://localhost:3000"
-#kubectl -n istio-system port-forward $(SERVICEGRAPH_POD_NAME) 8088:8088 & 
+kubectl -n istio-system port-forward $SERVICEGRAPH_POD_NAME 8088:8088 & 
 echo "Dotviz URL/Port: http://localhost:8088/dotviz"
-#kubectl -n istio-system port-forward $(PROMETHEUS_POD_NAME) 9090:9090 &
+kubectl -n istio-system port-forward $PROMETHEUS_POD_NAME 9090:9090 &
 echo "Prometheus URL/Port: http://localhost:9090/graph"
 
