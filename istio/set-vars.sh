ddf15ea8b8e19fe22f6c5a6ff018e25e2ce46acd
ctaguinod <ctaguinod@google.com>
2018-05-03 15:45:54 +0800

    updated vars

--- istio/set-vars.sh
+++ istio/set-vars.sh
@@ -2,6 +2,8 @@
 # GKE Cluster and GCP Project to Install Istio
 CLUSTER_NAME=dev
 PROJECT_ID=$USER-$CLUSTER_NAME
-
+# GCP Project with Container Registry
+SHARED_CLUSTER_NAME=shared
+SHARED_PROJECT_ID=$USER-$SHARED_CLUSTER_NAME
 # LATEST RELEASE: https://github.com/istio/istio/releases
 ISTIO_VERSION=0.7.1
