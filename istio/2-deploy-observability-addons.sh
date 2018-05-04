f01d19d00acdc8151f0a2652566254d2fb421282
ctaguinod <ctaguinod@google.com>
2018-05-04 15:10:19 +0800

    added jaeger-all-in-one-template.yml

--- istio/2-deploy-observability-addons.sh
+++ istio/2-deploy-observability-addons.sh
@@ -7,13 +7,19 @@ SHARED_CONTEXT=`kubectl config get-contexts | grep $CLUSTER_NAME | awk '{print $
 kubectl config use-context $SHARED_CONTEXT
 
 # Deploy observability addons
-# grafana
+# Grafana - port 3000
 kubectl apply -f istio-$ISTIO_VERSION/install/kubernetes/addons/grafana.yaml
-# prometheus
-kubectl apply -f istio-$ISTIO_VERSION/install/kubernetes/addons/prometheus.yaml
-# servicegraph
-kubectl apply -f istio-$ISTIO_VERSION/install/kubernetes/addons/servicegraph.yaml
-# zipkin
-kubectl apply -f istio-$ISTIO_VERSION/install/kubernetes/addons/zipkin.yaml
-kubectl apply -f istio-$ISTIO_VERSION/install/kubernetes/addons/zipkin-to-stackdriver.yaml
 
+# Prometheus - 9090
+kubectl apply -f istio-$ISTIO_VERSION/install/kubernetes/addons/prometheus.yaml
+
+# Servicegraph - 8088
+kubectl apply -f istio-$ISTIO_VERSION/install/kubernetes/addons/servicegraph.yaml
+
+# Zipkin - port 9411
+#kubectl apply -f istio-$ISTIO_VERSION/install/kubernetes/addons/zipkin.yaml
+#kubectl apply -f istio-$ISTIO_VERSION/install/kubernetes/addons/zipkin-to-stackdriver.yaml
+
+# Jaeger - port 16686
+#https://raw.githubusercontent.com/jaegertracing/jaeger-kubernetes/master/all-in-one/jaeger-all-in-one-template.yml
+kubectl apply -n istio-system -f jaeger-all-in-one-template.yml 
--- istio/4-port-forward-addons.sh
+++ istio/4-port-forward-addons.sh
@@ -8,17 +8,22 @@ kubectl config use-context $CONTEXT
 kubectl get nodes
 kubectl get ns
 
-ZIPKIN_POD_NAME=`kubectl -n istio-system get pod -l app=zipkin -o jsonpath='{.items[0].metadata.name}'`
-SERVICEGRAPH_POD_NAME=`kubectl -n istio-system get pod -l app=servicegraph -o jsonpath='{.items[0].metadata.name}'`
-GRAFANA_POD_NAME=`kubectl -n istio-system get pod -l app=grafana -o jsonpath='{.items[0].metadata.name}'`
-PROMETHEUS_POD_NAME=`kubectl -n istio-system get pod -l app=prometheus -o jsonpath='{.items[0].metadata.name}'`
+#ZIPKIN_POD_NAME=`kubectl -n istio-system get pod -l app=zipkin -o jsonpath='{.items[0].metadata.name}'`
+#kubectl -n istio-system port-forward $ZIPKIN_POD_NAME 9411:9411 & 
+#echo "Zipkin URL/Port: http://localhost:9411"
 
-kubectl -n istio-system port-forward $ZIPKIN_POD_NAME 9411:9411 & 
-echo "Zipkin URL/Port: http://localhost:9411"
-kubectl -n istio-system port-forward $GRAFANA_POD_NAME 3000:3000 &
-echo "Grafana URL/Port: http://localhost:3000"
+SERVICEGRAPH_POD_NAME=`kubectl -n istio-system get pod -l app=servicegraph -o jsonpath='{.items[0].metadata.name}'`
 kubectl -n istio-system port-forward $SERVICEGRAPH_POD_NAME 8088:8088 & 
 echo "Dotviz URL/Port: http://localhost:8088/dotviz"
+
+GRAFANA_POD_NAME=`kubectl -n istio-system get pod -l app=grafana -o jsonpath='{.items[0].metadata.name}'`
+kubectl -n istio-system port-forward $GRAFANA_POD_NAME 3000:3000 &
+echo "Grafana URL/Port: http://localhost:3000"
+
+PROMETHEUS_POD_NAME=`kubectl -n istio-system get pod -l app=prometheus -o jsonpath='{.items[0].metadata.name}'`
 kubectl -n istio-system port-forward $PROMETHEUS_POD_NAME 9090:9090 &
 echo "Prometheus URL/Port: http://localhost:9090/graph"
 
+JAEGER_POD_NAME=`kubectl -n istio-system get pod -l app=jaeger -o jsonpath='{.items[0].metadata.name}'`
+kubectl -n istio-system port-forward $JAEGER_POD_NAME 16686:16686 &
+echo "Prometheus URL/Port: http://localhost:16686"
